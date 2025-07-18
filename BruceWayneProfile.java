de3b0b80e2794b876da8c9f9da0f397cdcf630cf
JonathanWood00 <jonmark.wood00@gmail.com>
2025-07-18 18:21:36 -0500

    Initial commit: Bruce Wayne profile program

---
+++ BruceWayneProfile.java
@@ -0,0 +1,15 @@
+public class BruceWayneProfile {
+    public static void main(String[] args) {
+        String firstName = "Bruce";
+        String lastName = "Wayne";
+        String streetAddress = "1007 Mountain Drive";
+        String city = "Gotham City";
+        String zipCode = "53540";
+        
+        System.out.println("First Name: " + firstName);
+        System.out.println("Last Name: " + lastName);
+        System.out.println("Street Address: " + streetAddress);
+        System.out.println("City: " + city);
+        System.out.println("Zip Code: " + zipCode);
+    }
+}
